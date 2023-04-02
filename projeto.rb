098ba591d75cf62df5c6ad19135fcb391fa8ab7c
Guilherme lima <66636474+GuiDev182@users.noreply.github.com>
2023-04-02 13:04:27 -0300

    Primeiro Desafio

---
+++ projeto.rb
@@ -0,0 +1,15 @@
+# criar um programa na linguagem ruby que dê a saida do nome completo do usuario e a sua idade.
+
+#saida nome
+puts "digite seu nome completo: "
+
+#entrada nome
+nome = gets.chomp
+
+#saida idade
+puts "digite sua idade: "
+
+#entrada idade
+idade = gets.chomp
+
+puts "Olá, seu nome completo é #{nome} e a sua idade é #{idade}"
