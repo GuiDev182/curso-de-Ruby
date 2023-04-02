5a02546cbb7ab40db7a044c312ee1eace82cf45d
Guilherme lima <66636474+GuiDev182@users.noreply.github.com>
2023-04-02 13:03:11 -0300

    Aula 03
    aula 03 do curso de github

---
+++ aula 03.rb
@@ -0,0 +1,17 @@
+# 1) saída de dados na tela
+#  usuário digite seu nome
+
+puts "Digite seu nome: "
+
+
+# 2)programa permita que o usuário o que foi pedido
+# gets.chomp
+nome = gets.chomp
+
+puts "Digite seu sobrenome: "
+
+sobrenome = gets.chomp
+#3) saida final
+# para chamar um dado em ruby #{varivel}
+
+puts "oi, #{nome} #{sobrenome}!"
