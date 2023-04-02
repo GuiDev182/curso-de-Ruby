d5d17656769a8c3781349940bd2e38e4a5598698
Guilherme lima <66636474+GuiDev182@users.noreply.github.com>
2023-04-02 13:03:42 -0300

    aula 04
    aula 04 do curso de Ruby

---
+++ aula 04.rb
@@ -0,0 +1,13 @@
+
+# usuario digite 1 numero
+# pedir ao usuario
+# .to_i
+puts "digite um numero inteiro: "
+num1= gets.chomp.to_i
+# usuario digite outro numero
+puts "digite outro numero inteiro: "
+num2 = gets.chomp.to_i
+# some esses numeros do usuario
+soma = num1 + num2
+# apareça na tela
+puts "a soma dos numeros #{num1} e #{num2} é #{soma}"
