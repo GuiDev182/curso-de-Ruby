0f48bdbff7391619eeabf55480b720c9f4c3d13b
Guilherme lima <66636474+GuiDev182@users.noreply.github.com>
2023-04-02 16:59:43 -0300

    Aula de DoWhile(loop)

---
+++ DoWhile.rb
@@ -0,0 +1,10 @@
+#criar um contador
+# precisar de uma variavel
+count = 0
+loop do #loop de execução
+  puts count
+  if count == 150 # condição -> se (if) contador não for igual a 150
+    break
+  end
+  count += 1 #incremento -> somar 1
+end
