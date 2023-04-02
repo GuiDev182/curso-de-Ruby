903ab3928b36a9c05750da124dd69124ddd0b29d
Guilherme lima <66636474+GuiDev182@users.noreply.github.com>
2023-04-02 14:17:22 -0300

    Aula de for

---
+++ for.rb
@@ -0,0 +1,11 @@
+# leia uma lista(array) de elementos - linguagem de programação
+# primeiro - criar a lista
+linguagens = ['Ruby', 'Go', 'c++']
+count = 1
+# apareça na tela(puts) essa lista
+# laço for
+for linguagens in linguagens
+  puts linguagens
+  puts count
+  count += 1
+end
