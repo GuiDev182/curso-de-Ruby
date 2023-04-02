d8d3b781193240432ebca70706df5cab87b88a8e
Guilherme lima <66636474+GuiDev182@users.noreply.github.com>
2023-04-02 16:07:11 -0300

    Aula de times

---
+++ times.rb
@@ -0,0 +1,23 @@
+#exemplos 1 putz
+6.times {puts "oi"}
+#emplo 1 print
+6.times {print "oi "}
+#exemplo 2 puts
+4.times do
+  puts "oi"
+end
+
+#exemplo 2 print
+4.times do
+  print "oi"
+end
+
+#exemplo 3 contador puts
+20.times do |contador|
+  puts "#{contador}"
+end
+
+#exemplo 3 contador print
+20.times do |contador|
+  print "#{contador}"
+end
