53d82ac4aec4a6dd12855e9956a738c5c02d2bfa
Guilherme lima <66636474+GuiDev182@users.noreply.github.com>
2023-04-02 15:47:16 -0300

    Aula de while

---
+++ while.rb
@@ -0,0 +1,12 @@
+#comparar um numero com uma condição
+num = 1
+
+#imprima todos os numeros a partir de 1 e menos que 100
+# execute esse codigo ENQUANTO(WHILE), num for menor que 100
+
+while num < 100
+  puts num
+  #precisa pegar esse num que ele imrpimiu e seguir para o proximo
+  # utiliza o incremento
+  num += 1
+end
