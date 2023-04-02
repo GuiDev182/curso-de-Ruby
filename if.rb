0cb64addafcd67854ce248826cb574d7e0af5474
Guilherme lima <66636474+GuiDev182@users.noreply.github.com>
2023-04-02 13:37:55 -0300

    Aula de if,else e elsif

---
+++ if.rb
@@ -0,0 +1,12 @@
+#analise um dia da semana
+# Se esse dia da semana for domingo
+# imprima que o nosso almoço será especial
+dia = "feriado"
+
+if dia == "domingo" # == é uma comparação
+  puts "Nosso almoço será especial"
+elsif dia == "feriado"
+  puts "Nosso almoço será mais tarde"
+else
+  puts "Nosso almoço será normal"
+end
