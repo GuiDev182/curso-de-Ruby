3eb11b3e2e432fd7dbd8e97b5b2de7aa77ccb51d
Guilherme lima <66636474+GuiDev182@users.noreply.github.com>
2023-04-03 20:37:11 -0300

    Tipos de dados

---
+++ tipos de dados.rb
@@ -0,0 +1,22 @@
+#integer (inteiro)
+num = 1
+
+#float (decimais)
+pi = 3.1415349
+
+#boolean(verdadeiro/falso)
+
+verdadeiro = true
+
+#string (caracteres)
+nome = "guilherme"
+
+#array(vetor)
+nomes = ['joao','maria','josé']
+
+#symbol(string imutável)
+:joao
+
+#hash(coleção de dados)
+{nome: 'joao',cpf: '99999999999'}
+
