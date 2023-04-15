260384e24e5a0c078851e466cded6b2bcac08554
Guilherme lima <66636474+GuiDev182@users.noreply.github.com>
2023-04-15 12:05:25 -0300

    explorando gems

---
+++ bundler/Gemfile.rb
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+gem 'os'
+gem 'http', '~> 5.1', '>= 5.1.1'
+gem 'http-cookie', '~> 1.0', '>= 1.0.5'
---
+++ explorando gems.rb
@@ -0,0 +1,12 @@
+
+#Para instalar mais de uma gem ao mesmo tempo usamos:
+# bundler
+
+#passo a passo:
+# 1. Criar uma pasta nova, ex: bundler
+# 2. instalar o bundler na nossa máquina
+# para isso gem install bundler
+# dentro dessa pasta bundler vamos criar um arquivo chamado gemfile e vamos colocar:
+# source 'https://rubygems.org'
+# gem 'os'
+# gem (vamos pegar do site)
