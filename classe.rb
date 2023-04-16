d2f82a7da0ee81207475e770f3955b8b9546ca93
Guilherme lima <66636474+GuiDev182@users.noreply.github.com>
2023-04-16 15:53:55 -0300

    classe

---
+++ classe.rb
@@ -0,0 +1,7 @@
+
+#definindo classe:
+# A classe é a base para um objeto
+# exemplo: eu quero construir um prédio(objeto), para isso eu preciso de uma planta(classe)
+# resumindo, a classe é uma descrição de como será esse objeto
+# sozinha ela é apenas um desenho, mas junto com o objeto começamos a ver algo prático.
+# veremos as propriedades desse objeto(métodos), tamanho, qts quartos, quantos banheiros
