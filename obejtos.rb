d27c953502abd69f103218ed6174326fd3d466aa
Guilherme lima <66636474+GuiDev182@users.noreply.github.com>
2023-04-16 15:57:12 -0300

    objetos

---
+++ obejtos.rb
@@ -0,0 +1,7 @@
+#definição: tudo que for material e que puder ser percebida, um objeto do mundo real
+# ex: carro, casa, cachorro, avião, televisão, computador, livro
+# obs: todos os objetos apresemtam caracteristicas em comun:
+# comportamentos e informações
+# ex: cachorro
+# ex: informações: são os atributos: tamanho, cor, raça, peso
+# ex:comportamentos: são os metodos: latir, comer, morder
